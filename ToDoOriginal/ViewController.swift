d941e13ad600e6e1dd7acf0cb6b96345732fe1a9
Yukiyo Suenaga <xmasyukiyo@gmail.com>
2023-06-04 17:48:24 +0900

    表示できた！！！！！！やった！！！！いがりょうさんありがとうございます！！！

--- ToDoOriginal/ViewController.swift
+++ ToDoOriginal/ViewController.swift
@@ -10,12 +10,41 @@ import RealmSwift
 
 class ViewController: UIViewController {
     
-    @IBOutlet var MainLabel: UILabel!
+    var todoItem: Results<ToDo>!
     
-    @IBOutlet var DoneButton: UIButton!
-    @IBOutlet var SkipButton: UIButton!
+    
+    
+    @IBOutlet var todayLabel: UILabel!
+    
+    @IBOutlet var doneButton: UIButton!
+    @IBOutlet var skipButton: UIButton!
+    
+    var number: Int!
+    var todayTask: String!
+    
+    
+    var myData: [String] = ["Eat Breakfast", "Walk my dog", "Clean my shelf"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
         
-    
+            let realm = try! Realm()
+            todoItem = realm.objects(ToDo.self)
+        
+        let taskarray = Array(todoItem)
+
+        todayLabel.layer.cornerRadius = 25
+        
+        doneButton.layer.cornerRadius = 5
+        skipButton.layer.cornerRadius = 5
+        
+        number = Int.random(in: 0 ..< taskarray.count)
+        
+        todayTask = taskarray[number].title
+        
+        todayLabel.text = todayTask
+    }
 }
     
 extension ViewController: UITableViewDelegate {
@@ -23,6 +52,13 @@ extension ViewController: UITableViewDelegate {
         func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
             tableView.deselectRow(at: indexPath, animated: true)
         }
+    
+    @IBAction func doneButtonClicked() {
+        
+        todayLabel.text = "Have a wonderful day!"
+        
+    }
+    
     }
     
     
